7b740ba73f6dbb0ae6cfbc477ddf9d7e21abd09d
David Acevedo <rabbit.editor@yahoo.com>
2012-10-24 11:08:23 -0700

    Set up bottle name prompting :TESTONLY

--- c4p.rb
+++ c4p.rb
@@ -1,88 +1,79 @@
 #!/usr/bin/env ruby
 require 'optparse'
+
+def bottle_array(userpath)
+  bottles = Dir.glob("#{userpath}/.cxoffice/*").select { |i| File.directory?(i) }
+  Dir.glob("#{userpath}/.cxgames/*").select { |i| File.directory?(i) }.each do
+    bottles << i
+  end
+  bottles = bottles.map { |i| i.split('/').last }.reject do |i| 
+    i["tie"] or i["installers"] or i["desktopdata"] 
+  end
+  bottles
+end
+
 userpath = File.expand_path("~") #allows me to get user path.
+bottles = bottle_array(userpath)
+cx = "cxoffice"
+bottle_name = nil #THIS HAS TO BE THERE! why? closure.
 
 option_parser = OptionParser.new do |opts|
-  opts.banner = "You must at least supply a Bottle Name (in quotes) as an argument."
-  opts.on("-h", "--help", "Show this menu") { puts opts; exit }
-  opts.on("BOTTLE","-b BOTTLE", "--bottle" ) do |bottle| #add block to validate dir
-   #FileTest.directory?("#{userpath}/.#{cx}/#{bottle_name}") 
-   bottle_name = bottle
-   puts "Using #{bottle_name} bottle"
+  opts.banner = '#=> Example Usage: c4p.rb -b "Bottle Name"'
+  opts.on("-h", "--help", "Display this menu") { puts opts; exit }
+
+  opts.on("-b BOTTLE", "--bottle", bottles, 
+     "Assign a bottle to use from: \n\t\t#{bottles.join("\n\t\t")}") do |b| 
+   bottle_name = b
   end
-  opts.on("-g", "--games") do
-    if FileTest.directory?("#{userpath}/.cxgames") 
+
+  opts.on("-g", "--games", "Use CrossOver Games: old versions only") do
+    if File.directory?("#{userpath}/.cxgames") 
       cx = "cxgames"
     else 
       abort("Can't find Cross Over Games user Directory.")
     end
   end
-  
 end
-cx ||= "cxoffice"
+begin
+  option_parser.parse!
+rescue OptionParser::InvalidArgument
+  puts "Usage: c4p.rb -b 'Bottle Name'\nAvailable bottles: #{bottles.join(", ")}"
+  exit
+end
 
+puts "#####################"
+puts ARGV
+puts "#=> Using #{bottle_name} bottle"
+puts "#####################"
 # 
 # 
-# 
-# #Resolving Arguments
-# if ARGV[0].nil? 
-#   then puts "You must at least supply a Bottle Name (in quotes) as an argument." ; return
-# end
-# 
-# ARGV[1].nil? ? bottle_name = ARGV[0] : bottle_name = ARGV[1]
-# 
-# #Allows for this input -o Quicken 2011 without quotes.
-# unless ARGV[2].nil?
-#   bottle_name = bottle_name + " " + ARGV[2]
-#   puts bottle_name
-# end
-# 
-# #Figures out if you have only one cx and defaults to it otherwise uses flags
-# if ( FileTest.directory?("#{userpath}/.cxoffice") and FileTest.directory?("#{userpath}/.cxgames") )
-# then
-#   case ARGV[0].to_s
-#     when "-cxg", "-g"
-#       cx = "cxgames" 
-#     when "-cx", "-o"
-#       cx = "cxoffice"
-#     else 
-#       puts "Assuming you meant Cross Over (Office)." 
-#       cx = "cxoffice"
-#   end
-# elsif FileTest.directory?("#{userpath}/.cxoffice")
-#   cx = "cxoffice"
-#   puts "Assuming cxoffice."
-# elsif FileTest.directory?("#{userpath}/.cxgames") 
-#   cx = "cxgames"
-#   puts "Assuming cxgames."
-# else
-#   puts "Do you have CrossOver installed?"
-#   return
-# end
 
 #Check if bottle_name exists
-unless FileTest.directory?("#{userpath}/.#{cx}/#{bottle_name}") 
-  puts "ERROR: Directory #{bottle_name} doesn't exist."
-  puts "If your bottle name has spaces you must use quotes or add a -cx or -cxg flag."
-  return
-end
+#unless FileTest.directory?("#{userpath}/.#{cx}/#{bottle_name}") 
+#  puts "ERROR: Directory #{bottle_name} doesn't exist."
+#  exit
+#end
 
+if cx == "cxgames" #for testing only
 #/opt/ or other directory for cx office/games
 if FileTest.directory?("/opt/#{cx}")
   cxpath = "/opt/#{cx}" 
 elsif FileTest.directory?("#{userpath}/.#{cx}/bin/") 
   cxpath = "#{userpath}/.#{cx}"
 else 
-  puts "Can't find your cx wine directory."
-  return
+  abort("Can't find your cx wine directory.")
 end
+end #remove after test
 
-puts `#{cxpath}/bin/wine --bottle "#{bottle_name}" ~/.#{cx}/#{bottle_name}/drive_c/windows/system32/uninstaller.exe --list`
-
-
+#change %Q for %x for real use. Testing at the moment.
+puts %Q`#{cxpath}/bin/wine --bottle "#{bottle_name}" ~/.#{cx}/#{bottle_name}/drive_c/windows/system32/uninstaller.exe --list`
 
 #Goal: 
 # "(path to cxoffice directory) /opt/bin/wine --bottle "(bottle name)" 
 #  ~/.cxoffice/(bottle name)/drive_c/windows/system32/uninstaller.exe --list"
 
-
+__END__
+#Add the following directories to ~/.cxoffice/
+=begin
+["Spotify", "DAZ", "SumatraPDF", "Notepad++", "DragonStandard10", "Opera_1210", "Ephemeris", "SteamBloodlines", "7-Zip", "Quicken 2012"]
+=end
